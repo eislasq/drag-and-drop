30e25ce3afe78217b6bf67c128b664bc4eaf8b2c
Eliut Islas <eislasq@gmail.com>
2017-08-31 00:06:56 -0500

    Mejoras para permitir el uso de ng-strict-di Enhancements to allow use ng-strict-di

--- drag-and-drop.js
+++ drag-and-drop.js
@@ -6,26 +6,26 @@ var eiq_submodules_ = [];
             var submoduleName = 'drag-and-drop.directives.' + eventName;
             eiq_submodules_.push(submoduleName);
             angular.module(submoduleName, [])
-                    .directive(directiveName, function ($parse, $rootScope) {
-                        return {
-                            restrict: 'A',
-                            compile: function ($element, attr) {
-                                // We expose the powerful $event object on the scope that provides access to the Window,
-                                // etc. that isn't protected by the fast paths in $parse.  We explicitly request better
-                                // checks at the cost of speed since event handler expressions are not executed as
-                                // frequently as regular change detection.
-                                var fn = $parse(attr[directiveName], /* interceptorFn */ null, /* expensiveChecks */ true);
-                                return function ngEventHandler(scope, element) {
-                                    element.on(eventName, function (event) {
-                                        var callback = function () {
-                                            fn(scope, {$event: event});
-                                        };
-                                        scope.$apply(callback);
-                                    });
-                                };
-                            }
-                        };
-                    });
+                    .directive(directiveName, ['$parse', '$rootScope', function ($parse, $rootScope) {
+                            return {
+                                restrict: 'A',
+                                compile: function ($element, attr) {
+                                    // We expose the powerful $event object on the scope that provides access to the Window,
+                                    // etc. that isn't protected by the fast paths in $parse.  We explicitly request better
+                                    // checks at the cost of speed since event handler expressions are not executed as
+                                    // frequently as regular change detection.
+                                    var fn = $parse(attr[directiveName], /* interceptorFn */ null, /* expensiveChecks */ true);
+                                    return function ngEventHandler(scope, element) {
+                                        element.on(eventName, function (event) {
+                                            var callback = function () {
+                                                fn(scope, {$event: event});
+                                            };
+                                            scope.$apply(callback);
+                                        });
+                                    };
+                                }
+                            };
+                        }]);
         }
 );
 
